9eca1efe3fb95d0eeac498ea305b9568603c1967
rluispdev <rluisp@icloud.com>
2023-06-03 15:51:58 -0300

    Adicionar o .gitignore e o comite inicial

---
+++ TestandoGit/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TestandoGit
+//
+//  Created by Rafael Gonzaga on 03/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image("git1x")
+                    .resizable()
+                    .scaledToFit()
+                    .background()
+                    .cornerRadius(20)
+                    .padding()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+            
+            Text("Testantando Git + GitHub\n via Terminal")
+                .foregroundColor(.primary)
+                .multilineTextAlignment(.center)
+                .position(x:200, y: 320)
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestandoGit/TestandoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestandoGitApp.swift
+//  TestandoGit
+//
+//  Created by Rafael Gonzaga on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TestandoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
